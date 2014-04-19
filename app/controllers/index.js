8f9ac14c2859f923d3a5b997c410d3e087de89ca
Jennelle <nystromj@grinnell.edu>
2014-04-18 23:29:22 -0500

    add index controller

---
+++ app/controllers/index.js
@@ -0,0 +1,18 @@
+exports.index = function(req, res)
+{
+  
+  // Find out if the user has already logged in.
+  // If true, then redirect to the user's store page.
+  if (req.user)
+  {
+    res.redirect("/user/" + req.user.username);
+  }
+  else
+  {
+  }
+
+  res.render('index', { 
+  	title: "Welcome to CloudFlood - Let your users pay with tweets",
+  	messages: req.flash('login')
+  });
+};
