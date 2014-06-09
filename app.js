a7741a9114edfc611cc9916633af1f563b1916ab
Graeme Boy <graemeboy@gmail.com>
2014-06-09 14:12:25 +0200

    Change app.js to same as my blog

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require('./app/config/express')(app, passport, db);
 
 app.set('port', process.env.PORT || 3000);
 
-http.createServer(app).listen(app.get('port'), function () {
+var server = app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
