7b54742771b6bafadbd4314bb92430be9c86e667
Graeme Boy <graemeboy@gmail.com>
2014-06-09 15:17:17 +0200

    Revert db config

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,24 +4,28 @@ var express = require('express');
  * Controllers
  */
 
-var user = require('../controllers/user')
-  , index = require('../controllers/');
-  //, auth = require('./middlewares/authorization')
+var user = require('../controllers/user'),
+    index = require('../controllers/');
+//, auth = require('./middlewares/authorization')
 
 module.exports = function (passport) {
-  var router = express.Router();
+    var router = express.Router();
 
-  router.get('/logout', user.logout);
-  router.post('/process-login', passport.authenticate('local', {
-    failureRedirect: '/',
-    failureFlash: 'Invalid email or password.'
-  }), user.session);
-  router.post('/process-signup', user.create);
-  
+    router.get('/logout', user.logout);
+    router.post('/process-login', passport.authenticate('local', {
+        failureRedirect: '/',
+        failureFlash: 'Invalid email or password.'
+    }), user.session);
+    router.post('/process-signup', user.create);
 
-  router.use('/dashboard', require('./dashboard'));
-  router.use('/campaign', require('./campaign'));
-  router.get('/', index.index);
-  
-  return router;
+
+    router.use('/dashboard', require('./dashboard'));
+    router.use('/campaign', require('./campaign'));
+
+    // Make a landing page for beta
+
+    router.get('/try', index.beta);
+    router.get('/', index.index);
+
+    return router;
 }
